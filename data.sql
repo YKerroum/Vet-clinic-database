82cc05cf0036d93d410780acc0a27af122c1c010
Youssef KERROUM <78832703+YKerroum@users.noreply.github.com>
2023-01-17 00:56:54 +0100

    Created database and table animals added

--- data.sql
+++ data.sql
@@ -1,5 +1,5 @@
 /* Populate database with sample data. */
 
-INSERT INTO animals (name) VALUES ('Luna');
+INSERT INTO animals (name,date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Agumon',);
 INSERT INTO animals (name) VALUES ('Daisy');
 INSERT INTO animals (name) VALUES ('Charlie');
